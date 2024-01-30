2dfccc862c4e356020e290d212a53f1d552614ab
gerry132 <61889626+gerry132@users.noreply.github.com>
2024-01-30 16:46:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,40 +18,6 @@ resource "aws_db_subnet_group" "default" {
   subnet_ids    = var.subnets
 }
 
-
-resource "aws_db_parameter_group" "default-15" {
-  name   = "pg-15-parameter-group"
-  family = "postgres15"
-
-  parameter {
-    name  = "lc_messages"
-    value = "en_IE.UTF-8"
-  }
-
-  parameter {
-    name  = "lc_monetary"
-    value = "en_IE.UTF-8"
-  }
-
-  parameter {
-    name  = "lc_numeric"
-    value = "en_IE.UTF-8"
-  }
-
-  parameter {
-    name  = "lc_time"
-    value = "en_IE.UTF-8"
-  }
-
-  parameter {
-    apply_method = "pending-reboot"
-    name         = "shared_buffers"
-    value        = "{DBInstanceClassMemory/32768}"
-  }
-}
-
-
-
 resource "aws_db_parameter_group" "default" {
   name   = "${var.name}-parameter-group"
   family = "postgres12"
